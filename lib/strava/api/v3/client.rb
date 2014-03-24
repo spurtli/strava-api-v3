9acf8a46d06e950c06a1e1e3273533fd61ea85d3
James Chevalier <jchevalier@gmail.com>
2014-03-24 18:51:46 -0400

    add stream read requests
    Add access to these endpoints:
    * Retrieve activity streams
    * Retrieve effort streams
    * Retrieve segment streams

--- lib/strava/api/v3/client.rb
+++ lib/strava/api/v3/client.rb
@@ -2,6 +2,7 @@ require 'strava/api/v3/athlete'
 require 'strava/api/v3/activity'
 require 'strava/api/v3/gear'
 require 'strava/api/v3/segment'
+require 'strava/api/v3/stream'
 
 module Strava::Api::V3
 
@@ -11,6 +12,7 @@ module Strava::Api::V3
     include Activity
     include Gear
     include Segment
+    include Stream
 
     # Define the same set of accessors as the Awesome module
     attr_accessor *Configuration::VALID_CONFIG_KEYS
