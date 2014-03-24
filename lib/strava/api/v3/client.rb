1e1dca58cb4ece623ecc27f8b57cbc5f34d51977
James Chevalier <jchevalier@gmail.com>
2014-03-24 18:32:29 -0400

    add activity read requests
    Add access to these endpoints:
    * retrieve an activity
    * list athlete activities
    * list friends' activities
    * list activity zones
    * list activity laps
    
    This commit also includes some minor code formatting

--- lib/strava/api/v3/client.rb
+++ lib/strava/api/v3/client.rb
@@ -1,19 +1,21 @@
 require 'strava/api/v3/athlete'
+require 'strava/api/v3/activity'
 
 module Strava::Api::V3
- 
+
   class Client
 
     include Athlete
- 
+    include Activity
+
     # Define the same set of accessors as the Awesome module
     attr_accessor *Configuration::VALID_CONFIG_KEYS
- 
+
     def initialize(options={})
       # Merge the config values from the module and those passed
       # to the client.
       merged_options = Strava::Api::V3.options.merge(options)
- 
+
       # Copy the merged values to this client and ignore those
       # not part of our configuration
       Configuration::VALID_CONFIG_KEYS.each do |key|
@@ -22,7 +24,7 @@ module Strava::Api::V3
 
       puts self.access_token
     end
- 
+
   end # Client
- 
+
 end
