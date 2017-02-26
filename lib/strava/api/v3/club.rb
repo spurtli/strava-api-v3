18f73e500b8ece8d4c16def6a229e9071c30f08a
Antonio Leal <tonyjleal@iMac-de-Antonio.local>
2017-02-26 21:55:57 +0100

    Added list club announcements and list club admins endpoints

--- lib/strava/api/v3/club.rb
+++ lib/strava/api/v3/club.rb
@@ -32,6 +32,20 @@ module Strava::Api::V3
       api_call('athlete/clubs', args, 'get', options, &block)
     end
 
+    # Announcements are posts sent by Club Admins or Owners to the members of a club. Only members of private clubs can access their announcements. The objects are returned in summary representation.
+    # 
+    # See {http://strava.github.io/api/v3/clubs/#get-announcements} for full details
+    # 
+    # @param id required
+    # @param args any additional arguments
+    # @param options (see #get_object)
+    # @param block post processing code block
+    # 
+    # @return an array of club announcement summary representations.
+    def list_club_announcements(id, args = {}, option = {}, &block)
+        api_call("clubs/#{id}/announcements", args, 'get', option, &block)
+    end
+
     # Fetch information about the members of a specific club
     #
     # See {http://strava.github.io/api/v3/clubs/} for full details
@@ -44,6 +58,20 @@ module Strava::Api::V3
     def list_club_members(id, args = {}, options = {}, &block)
       # Fetches the connections for given object.
       api_call("clubs/#{id}/members", args, 'get', options, &block)
+    end
+
+    # Fetch information about the admins of a specific club
+    #
+    # See {http://strava.github.io/api/v3/clubs/} for full details
+    #
+    # @param args any additional arguments
+    # @param options (see #get_object)
+    # @param block post processing code block
+    #
+    # @return club json (see http://strava.github.io/api/v3/clubs/)
+    def list_club_admins(id, args = {}, options = {}, &block)
+      # Fetches the connections for given object.
+      api_call("clubs/#{id}/admins", args, 'get', options, &block)
     end
 
     # Fetch information about the activities of a specific club
